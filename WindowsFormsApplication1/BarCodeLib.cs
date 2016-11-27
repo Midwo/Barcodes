83f9820991f09264e569f7a9f5a07418ef05fbb2
Michał <michal.dwojak92@gmail.com>
2016-11-27 23:49:30 +0100

    Add project files.

---
+++ WindowsFormsApplication1/BarCodeLib.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BarcodeLib;
+
+namespace WindowsFormsApplication1
+{
+    class BarCodeLib
+    {
+
+    }
+}
